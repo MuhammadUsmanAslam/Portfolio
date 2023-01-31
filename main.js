f63212551ce9364991db0a03bb22ba5279a1b3f6
TaiwoEnoch <taiwoenoch0@gmail.com>
2023-01-31 15:33:29 +0100

    Add details popup mobile and desktop version design.

--- main.js
+++ main.js
@@ -14,3 +14,16 @@ hideNav.addEventListener('click', () => {
 mobileMenuLinks.addEventListener('click', () => {
   mobileMenu.classList.toggle('mobile-menu--show');
 });
+// Details popup starts here
+const seeProject = document.getElementById('see-project');
+const detailCard = document.getElementById('detail-card');
+
+seeProject.addEventListener('click', () => {
+  detailCard.classList.toggle('detail-card--show');
+});
+
+const detailCardClose = document.getElementById('detail-card-close');
+detailCardClose.addEventListener('click', () => {
+  detailCard.classList.toggle('detail-card--show');
+});
+// Details popup ends here
