a7e44dce1d0838dd6a9ff6ab2263c34287a9d82b
TaiwoEnoch <taiwoenoch0@gmail.com>
2023-02-03 15:31:00 +0100

    Add storing input to the local storage.

--- main.js
+++ main.js
@@ -206,5 +206,11 @@ const keys = Object.keys(contactFormData);
 for (let i = 0; i < keys.length; i += 1) {
   contactForm.elements[keys[i]].value = contactFormData[keys[i]];
 }
-
+// This function updates data in localStorage when user add data in contact form
+for(let i=0; i < contactForm.length; i++){
+  contactForm.elements[i].addEventListener('change', (e) => {
+    contactFormData[`${contactForm.elements[i].name}`] = e.target.value;
+    window.localStorage.setItem("contactFormData", JSON.stringify(contactFormData));
+  });
+}
 // LocalStorage Ends Here
